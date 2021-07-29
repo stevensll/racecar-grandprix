1d5bc2cb06168c6bd3789d935bff257b854d7262
stevensll <deltaorb637@gmail.com>
2021-07-29 10:28:23 -0400

    trying to fix turning

--- custom_funcs.py
+++ custom_funcs.py
@@ -17,7 +17,7 @@ def get_contour_info(image, colorStart: Tuple[int,int], colorEnd:Tuple[int,int],
         2) The area of the detected contour
         3) The modified image with a draw function appied
     """
-    contour_center = None
+    contour_center = [0,0]
     contour_area = 0
     contour = None
 
@@ -35,3 +35,14 @@ def get_contour_info(image, colorStart: Tuple[int,int], colorEnd:Tuple[int,int],
     
     return contour_center,contour_area,cropped_image
 
+def get_n_contour_info(n,image, colorStart: Tuple[int,int], colorEnd:Tuple[int,int], CROP_FLOOR, MIN_CONTOUR_AREA = 150):
+
+    cropped_image = rc_utils.crop(image, CROP_FLOOR[0], CROP_FLOOR[1])
+    contours = rc_utils.find_contours(cropped_image, colorStart, colorEnd)
+    contours.sort(reverse = True, key = lambda x: rc_utils.get_contour_area(x))
+
+    lst = []
+    for i in range(n):
+        if contours is not None and i in range(0, len(contours)):
+            lst.append([contours[i], rc_utils.get_contour_center(contours[i]),rc_utils.get_contour_area(contours[i])])
+    return lst
--- main-grandprix.py
+++ main-grandprix.py
@@ -26,13 +26,13 @@ class State(IntEnum) :
 
 
 # Color HSV
-CROP_FLOOR = ((0, 0), (rc.camera.get_height(), rc.camera.get_width()))
+CROP_FLOOR = ((300, 0), (rc.camera.get_height(), rc.camera.get_width()))
 
 BLUE = ((90, 200, 200), (120, 255, 255), "blue")  
 GREEN = ((40,50,50), (80,255,255), "green")
 RED = ((170,50,50), (10,255,255), "red")
-ORANGE = ((15,200,200), (35,255,255,),"orange")
-PURPLE = ((125,50,50),(140,255,255), "purple")
+ORANGE = ((15,230,230), (35,255,255,),"orange")
+PURPLE = ((125,200,200),(140,255,255), "purple")
 
 
 RIGHT_WINDOW = (32,38)
@@ -44,6 +44,8 @@ MIN_CONTOUR_AREA = 30
 speed = 0.0  # The current speed of the car
 angle = 0.0  # The current angle of the car's wheels
 
+right_center = [0,0]
+left_center = [0,0]
 
 
 def start():
@@ -53,56 +55,84 @@ def start():
 def update():
     global speed
     global angle
+    global right_center
+    global left_center
     image = rc.camera.get_color_image()
-    
-    # center,area,image = rc_cf.get_contour_info(image, ORANGE[0], ORANGE[1], CROP_FLOOR)
-    
-    # rc.display.show_color_image(image)
-    # if center is not None:
-    #     angle  = rc_utils.remap_range(center[1],0, rc.camera.get_width(), -1,1, True)
-    
-    # speed = 1
-   
+
     color = PURPLE  
-
-
-    left_image =image[0:rc.camera.get_height(),0:rc.camera.get_width()//2]
-    right_image = image[0:rc.camera.get_height(),rc.camera.get_width()//2:rc.camera.get_width()-1]
-
-    left_line_center,area,left_cropped = rc_cf.get_contour_info(left_image, color[0],color[1],CROP_FLOOR,50)
-    right_line_center,area,right_cropped = rc_cf.get_contour_info(right_image, color[0], color[1], CROP_FLOOR,50)
-
-
-    # rc_utils.draw_circle(left_cropped,left_line_center)
-    # rc_utils.draw_circle(right_cropped,right_line_center)
+    turn_priority = "PURPLE"
+    
     cropped_image = rc_utils.crop(image, CROP_FLOOR[0], CROP_FLOOR[1])
 
-    if left_line_center or right_line_center is not None:
-        right_line_center[1]+=rc.camera.get_width()//2
-        center = [(left_line_center[0] + right_line_center[0] )// 2,(left_line_center[1] + right_line_center[1])// 2]
-        # center = [(left_line_center[0]),(left_line_center[1] + 300)]
-        rc_utils.draw_circle(cropped_image, left_line_center)
-        rc_utils.draw_circle(cropped_image, right_line_center)
+    purple_contours = rc_utils.find_contours(cropped_image, PURPLE[0], PURPLE[1])
+    orange_contours = rc_utils.find_contours(cropped_image, ORANGE[0], ORANGE[1])
+    
+    # if purple_contours:
+    #     purple_area = rc_utils.get_contour_area(rc_utils.get_largest_contour(purple_contours, MIN_CONTOUR_AREA))
+    # else: 
+    #     purple_area = 0
+    
+    # if orange_contours:
+    #     orange_area = rc_utils.get_contour_area(rc_utils.get_largest_contour(orange_contours, MIN_CONTOUR_AREA))
+    # else:
+    #     orange_area = 0
 
-        rc_utils.draw_circle(cropped_image, center)
-        rc.display.show_color_image(cropped_image)
-        
-        angle  = rc_utils.remap_range(center[1],0, rc.camera.get_width(), -1,1, True)
+    if turn_priority == "PURPLE":
+        if purple_contours: 
+            print("purple")
+            rc_utils.draw_contour(cropped_image, purple_contours[0])
+            color = PURPLE
+        else: 
+            color = ORANGE
 
 
+
+
+    contours = rc_cf.get_n_contour_info(2,image,color[0],color[1], CROP_FLOOR)
+    
+
+
+    if contours is not None:
+        print(len(contours))
+        if len(contours) >= 1:
+            if contours[0] is not None and contours[0][1] is not None:
+                right_center = contours[0][1]
+
+        if len(contours) == 2:
+            if contours [1] is not None and contours[1][1] is not None:
+                left_center = contours[1][1]
+                
+    center = [(right_center[0] + left_center[0]) // 2, (right_center[1] + left_center[1])//2]
+
+    rc_utils.draw_circle(cropped_image, left_center)
+    rc_utils.draw_circle(cropped_image, right_center)
+    rc_utils.draw_circle(cropped_image, center)
+
+
+
+    rc.display.show_color_image(cropped_image)
+
+    
+    
+    
+    
+    lt = rc.controller.Trigger.LEFT
+    rt = rc.controller.Trigger.RIGHT
+    
+    speed = rc.controller.get_trigger(rt)-rc.controller.get_trigger(lt)
+    angle = rc.controller.get_joystick(rc.controller.Joystick.LEFT)[0]
+    
     speed = 0.5
+    angle = rc_utils.remap_range(center[1],0,rc.camera.get_width(),-1,1,True)
 
     rc.drive.set_speed_angle(speed, angle)
-    
     if rc.controller.is_down(rc.controller.Button.B):
-        print(speed)
+        print(angle)
         # if green_center is None:
         #     print("No green contour found")
         # else:
         #     print(green_center, speed)
-
-
-
+    
 if __name__ == "__main__":
     rc.set_start_update(start, update, None)
     rc.go()
